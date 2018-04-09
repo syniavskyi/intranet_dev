23e5cf9e55db0de764addb556d1e2ae7f4b3d1d1
Roman Protas <r.protas93@gmail.com>
2018-04-09 12:34:57 +0200

    Initial commit

--- src/main/java/com/btech/oauth2/InitApp.java
+++ src/main/java/com/btech/oauth2/InitApp.java
@@ -22,9 +22,9 @@ public class InitApp {
 	@Autowired
 	public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository repo, UserService userService) throws Exception {
 		//if necessary add user manually
-		/*		if(repo.count() == 0) {
-					userService.save(new User("admin", "password", Arrays.asList(new Role("USER"), new Role("ACTUATOR") , new Role("ADMIN"))));
-			}*/
+		/*if(repo.count() == 0) {
+			userService.save(new User("admin", "password", Arrays.asList(new Role("USER"), new Role("ACTUATOR") , new Role("ADMIN"))));
+		}*/
 		builder.userDetailsService (s -> new CustomUserDetails(repo.findByUsername(s)));
 	}
 	
