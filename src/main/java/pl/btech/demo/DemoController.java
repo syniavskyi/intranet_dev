98d6862c32335de9247f3c255ae0bf7951841fb6
syniavskyi <syniavskyi.pavlo@gmail.com>
2018-04-10 10:19:14 +0200

    Added new content

---
+++ src/main/java/pl/btech/demo/DemoController.java
@@ -0,0 +1,14 @@
+package pl.btech.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index.html";
+    }
+
+}
