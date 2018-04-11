4f1e45bdd0e8e7c1dbaac4e07350dfaa2d4c2928
Maria Harcej <maria.harcej@btech.pl>
2018-04-11 12:06:35 +0200

    Added v-model binding
    Added v-model properties to inputs, data properties and basic onSubmit method

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,35 +2,43 @@
 	<div class="wrapper">
         <div class="plane--left--parent">
                 <div class="plane--left">
-                    <h1 class="intranet--title">{{ Hello }}</h1>
+                    <h1 class="intranet--title">Witaj</h1>
                     <div class="image--user"></div>
-                    <p class="intranet--subtitle">{{ login }}</p>
+                    <p class="intranet--subtitle">Zaloguj się do Intranetu</p>
                 </div>
-         </div>
+        </div>
 <!--            <div class="clearfix"></div>-->
-           <div class="plane--right--parent">
+        <div class="plane--right--parent">
                 <div class="plane--right">
                     <div class="image--logo"></div>
                     <div class="login--credentials">
-                        <input class="input" type="email"/>
-                        <label for="password" class="label plane--right__label1">Użytkownik</label>
-                        <input class="input" id="password" type="password" />
-                        <label class="label plane--right__label2">Hasło</label>
+                        <input class="input" id="username" type="email" v-model="username"/>
+                        <label class="label plane--right__label1" for="username">Użytkownik</label>
+                        <input class="input" id="password" type="password" v-model="password"/>
+                        <label class="label plane--right__label2" for="password" >Hasło</label>
                     </div>
-           </div>
+        </div>
         </div>
     </div>
 </template>
 
 <script>
 	export default {
-	  name: 'Login',
-	  data () {
-	    return {
-	      Hello: "Witaj",
-	      login: "Zaloguj się do Intranetu"
-	    }
-	  }
+	    name: 'Login',
+	    data () {
+	        return {
+                username: '',
+                password: ''
+	        }
+        },
+        methods: {
+            onSubmit() {
+                this.$store.dispatch('login', {
+                    username: this.username,
+                    password: this.password
+                })
+            }
+        }
 	}
 </script>
 
