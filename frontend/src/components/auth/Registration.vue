60dd5efb4f8529a81ea93176cfad168bf9093632
Kacper Borkowski <k.borkowski0@gmail.com>
2018-04-12 11:01:12 +0200

    Changed form
    Changed tamplate in form

--- frontend/src/components/auth/Registration.vue
+++ frontend/src/components/auth/Registration.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="plane-parent">
-        <label for="">Imię i nazwisko</label>
-        <input type="text" v-model="fullName">
-        <label for="">E-mail</label>
-        <input type="email" disabled="false" v-model="fullNameToEmail">
-        <!-- <p>{{ fullName }}</p> -->
-        <label for="">Hasło</label>
-        <input type="password">
-        <button disabled="disabled">Wygeneruj hasło</button>
-        <label for="">Rola</label>
-        <select name="" id="">
-            <option value="">Management</option>
-            <option value="">Leader</option>
-            <option value="">Office</option>
-            <option value="">Basic</option>
-        </select>
-        <label for="">Oddział</label>
-        <select name="" id="">
-            <option value="">Dąbrowa Górnicza</option>
-            <option value="">Wrocław</option>
-        </select>
+        <div class="plane">
+            <div class="plane-left">
+                <img class="img-user" src="../../assets/images/grouper-256.png">
+                <p class="p-login">Zarejestruj pracownika w Intranecie</p>
+            </div>
+            <div class="login-credentials">
+                <input type="text" name="fullName" v-model="fullName">
+                <label for="fullName">Imię i nazwisko</label>
+                <label for="">E-mail</label>
+                <input type="email" disabled="false" v-model="fullNameToEmail">
+                <!-- <p>{{ fullName }}</p> -->
+                <label for="">Hasło</label>
+                <input type="password">
+                <button disabled="disabled">Wygeneruj hasło</button>
+                <label for="">Rola</label>
+                <select name="" id="">
+                    <option value="">Management</option>
+                    <option value="">Leader</option>
+                    <option value="">Office</option>
+                    <option value="">Basic</option>
+                </select>
+                <label for="">Oddział</label>
+                <select name="" id="">
+                    <option value="">Dąbrowa Górnicza</option>
+                    <option value="">Wrocław</option>
+                </select>
+                <button class="button"><span>Zarejestruj</span></button>
+            </div>
+        </div>
     </div>
 </template>
 
