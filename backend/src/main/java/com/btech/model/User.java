04a919ea5c1344deb0067348d958447d6c728e6e
Roman Protas <r.protas93@gmail.com>
2018-04-12 11:12:17 +0200

    Roles and registration updated

--- backend/src/main/java/com/btech/model/User.java
+++ backend/src/main/java/com/btech/model/User.java
@@ -18,18 +18,21 @@ public class User {
     @Column(name="password")
     @JsonIgnore
     private String password;
+    @JsonIgnore
     @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
     private List<Role> roles;
     @Column(name="email")
     private String email;
+    @Column(name="role_id")
+    private int roleId;
 
     public User() {}
 
-    public User(String username, String password, String passwordConfirmation, List<Role> roles, String email) {
+    public User(String username, String password, String passwordConfirmation, String email, int roleId) {
         this.username = username;
         this.password = password;
-        this.roles = roles;
         this.email = email;
+        this.roleId = roleId;
     }
 
     public String getUsername() {
@@ -62,6 +65,14 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
 	}
     
 }
