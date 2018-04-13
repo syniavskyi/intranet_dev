b0e3cc2c3493af781efea04a3892eb0d759e7eda
Kacper Borkowski <k.borkowski0@gmail.com>
2018-04-13 16:26:40 +0200

    Comment mode

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,7 +7,7 @@ import Dashboard from '@/components/Dashboard'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     routes: [{
         path: '/',
         name: 'Login',
