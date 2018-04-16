e946dab8a1a3fb07073267b8b1121b26f240c15d
Maria Harcej <maria.harcej@btech.pl>
2018-04-16 15:05:54 +0200

    Changed logout method
    Changed method logout in login.js and added tes button in dashboard.vue

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
       <p>dashboard</p>
+      <button class="button login-button"  @click="logout"><span class="span-arrow">Wyloguj (test)</span></button>
+      
   </div>
 </template>
 
 
 <script>
-import axios from 'axios'
+
+   
+
+	export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch('logout')
+      }
+    }
+  }
 </script>
