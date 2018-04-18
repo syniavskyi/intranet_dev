02848568eff97d428cf84faa5c157c492485eb55
Maria Harcej <maria.harcej@btech.pl>
2018-04-18 13:28:48 +0200

    Added animation
    added slide animation for going from login to dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- <div id="app"> -->
+  <transition name="page" mode="out-in">
     <router-view/>
+  </transition>
   <!-- </div> -->
 </template>
 
@@ -11,5 +13,26 @@ export default {
 </script>
 
 <style>
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .10s;
+}
+
+.fade-enter-active {
+  transition-delay: .01s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+
+.page-enter-active, .page-leave-active {
+  
+  transition: opacity .15s, transform .15s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
 
 </style>
