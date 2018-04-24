4b61c529dfd2782e797cab86871f5642cdcaa242
Kacper Borkowski <k.borkowski0@gmail.com>
2018-04-24 16:05:00 +0200

    Added employees tab
    Added list of employees

--- frontend/src/components/content/Employees.vue
+++ frontend/src/components/content/Employees.vue
@@ -1,15 +1,77 @@
 <template>
-<div id="news">
-  <div class="plane-dashboard-parent">
-            <div class="plane-dashboard">
-                <div class="plane-dashboard-nav-and-content">
-                    <div class="dashboard-content">
-              <p>Employees</p>
-           </div>
-      </div>
+  <div id="container">
+    <div>
+      <p>Imie i nazwisko</p>
+      <ul>
+        <li v-for="userInfo in getUserList">
+          {{ userInfo.firstName }} {{ userInfo.lastName }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>Stanowisko</p>
+      <ul>
+        <li v-for="userInfo in getUserList">
+          {{ userInfo.roleName }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>Oddział</p>
+      <ul>
+        <li v-for="userInfo in getUserList">
+          {{ userInfo.depName }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>Telefon</p>
+      <ul>
+        <li v-for="userInfo in getUserList">
+          {{ userInfo.phone }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>E-mail</p>
+      <ul>
+        <li v-for="userInfo in getUserList"><a :href="'mailto:' + userInfo.email">
+          {{ userInfo.email }}
+        </a></li>
+      </ul>
     </div>
   </div>
-</div>
 </template>
+
+<script>
+import axios from "axios";
+  export default {
+    data() {
+      return {
+        userInfo: {}
+      }
+    },
+    beforeCreate() {
+      this.$store.dispatch("getUserInfo");
+    },
+    computed: {
+      getUserList() {
+        return this.$store.getters.usersList;
+      }
+    }
+  }
+</script>
+
 <style>
+#container {
+  display: flex;
+}
+
+p {
+  padding: 5px 15px;
+}
+
+ul {
+  padding: 5px 15px;
+}
 </style>
