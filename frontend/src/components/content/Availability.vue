ee84b7d7d541b7e3c1b9d179cd2e955e9e072a32
Maria <marysia.harcej@gmail.com>
2018-05-14 08:58:17 +0200

    Added availability basics
    Created files for availability, added routing

---
+++ frontend/src/components/content/Availability.vue
@@ -0,0 +1,30 @@
+<<template>
+    <div>
+        <p>Dysp</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+      return {
+ 
+      }
+    },
+    beforeCreate() {
+        if (this.showMenu === false) {
+            this.$store.commit('DISPLAY_MENU', true)
+        }
+    },
+    created() {
+
+	},
+    methods: {
+      
+    }
+  }
+</script>
+<style>
+
+</style>
