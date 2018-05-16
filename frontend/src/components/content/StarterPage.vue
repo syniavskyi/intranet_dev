c45d5c4e0f97d6daece452a7444a31c92afa93b2
Kacper Borkowski <k.borkowski0@gmail.com>
2018-05-16 09:57:18 +0200

    Added checkbox handling

--- frontend/src/components/content/StarterPage.vue
+++ frontend/src/components/content/StarterPage.vue
@@ -1,13 +1,83 @@
 <template>
-  <h1>Test</h1>
+  <div>
+    <h1>Starter Page</h1>
+    <h3>Witaj w firmie! Poniżej lista dokumentów do wypełnienia i dostarczenia dla Back Office'u dla nowozatrudnionych pracowników.</h3>
+    <ul>
+      <li v-for="list in listOfDoc" :class="list.checked ? 'line-through' : 'none' ">
+        <!-- <button>Plik</button>{{ list }}<input type="checkbox" @change="$v.docCheckbox.$touch()" v-model="docCheckbox"> -->
+        <button>Plik</button>{{ list.title }}<input type="checkbox" :checked="list.checked" @change="test(list)">
+      </li>
+    </ul>
+    <button>Dostarczyłem wszystkie dokumenty</button>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { required } from 'vuelidate/lib/validators'
+import i18n from '../../lang/lang'
+
 export default {
-  
+  data() {
+    return {
+      listOfDoc: [
+        {
+          title: "Umowa o Pracę",
+          checked: false
+        },
+        // "Umowa o Pracę",
+        {
+          title: "Zakres obowiązków",
+          checked: true
+        }
+        // "Zakres obowiązków",
+        // "Informacja dla praacownika",
+        // "Deklaracja PIT-2",
+        // "Dane personalne",
+        // "Oświadczenie dla Urzędu Skarbowego",
+        // "Oświadczenie zleceniobiorcy",
+        // "Kwestionariusz osobowy",
+        // "Oświadczenie na rozłąkę",
+        // "Dotychczasowe świadectwa pracy",
+        // "Dyplomy",
+        // "Dokumentacja od lekarza tj. badania okresowe i psychologiczne pod kątem prowadzenia auta"
+      ],
+      // listOfDoc: [
+      //   {
+      //     title: "test",
+      //     checkbox: false
+      //   },
+      //   {
+      //     title: "test2",
+      //     checkbox: false
+      //   }
+      // ]
+    }
+  },
+  methods: {
+    test(data) {
+      data.checked = !data.checked;
+    }
+  }
+  // ,
+  // validations: {
+  //   listOfDoc: {
+  //     // required
+  //     checkbox: {
+  //       required
+  //     }
+  //   }
+  // }
 }
 </script>
 
 <style scoped>
+  .line-through {
+    text-decoration: line-through;
+    color: grey;
+  }
 
+  .none {
+    text-decoration: none;
+  }
 </style>
