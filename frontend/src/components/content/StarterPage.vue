3148a7c992054f3e246f56869b29a023dca647e0
Kacper Borkowski <k.borkowski0@gmail.com>
2018-05-16 11:55:42 +0200

    Added styles for starter page

--- frontend/src/components/content/StarterPage.vue
+++ frontend/src/components/content/StarterPage.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    <h1>Starter Page</h1>
-    <h3>Witaj w firmie! Poniżej lista dokumentów do wypełnienia i dostarczenia dla Back Office'u dla nowozatrudnionych pracowników.</h3>
-    <ul>
-      <li v-for="list in listOfDoc" :class="list.checked ? 'line-through' : 'none'">
-        <button>Plik</button>{{ list.title }}<input type="checkbox" :checked="list.checked" @change="changeCheckbox(list)">
-      </li>
-    </ul>
-    <button :disabled="setButton">Dostarczyłem wszystkie dokumenty</button>
+  <div class="plane-starter-page">
+    <div class="starter-page-nav-and-content">
+      <div class="starter-page-content">
+        <div class="starter-header"> 
+          <div class="starter-header-title-and-menu">
+            <img src="../../assets/images/nav/if_menu-32.png" width="32px" class="starter-header-menu">
+            <p class="starter-header-title">Strona Startowa</p>
+          </div>
+        </div>
+        <h3 class="starter-page-user-header">Witaj w BTech!</h3>
+        <div class="starter-page-list">
+          <div class="starter-page-list-header">
+            <p class="starter-page-list-title">Poniżej znajduje się lista dokumentów które musisz wypełnić i dostarczyć</p>
+          </div>
+          <div class="starter-page-list-content">
+            <ul class="starter-page-ul">
+              <li class="starter-page-item" v-for="list in listOfDoc">
+                <div class="starter-page-list-item-btns">
+                  <button class="starter-page-file-btn">&#x21e3;</button>
+                  <div class="starter-page-pdf">.pdf</div>
+                </div>
+                <div class="starter-page-list-item-wrapper">
+                  <div class="starter-page-item-text"  :class="list.checked ? 'line-through' : 'none'">
+                    {{ list.title }}
+                  </div>
+                </div>
+                <input class="starter-page-checkbox" :checked="list.checked" @change="changeCheckbox(list)" type="checkbox">
+              </li>
+            </ul>
+            <div class="starter-page-list-bottom">
+              <button class="starter-page-docs-btn button" :disabled="setButton">Dokumenty przekazano</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,22 +50,50 @@ export default {
           title: "Umowa o Pracę",
           checked: false
         },
-        // "Umowa o Pracę",
         {
           title: "Zakres obowiązków",
           checked: true
+        },
+        {
+          title: "Informacja dla praacownika",
+          checked: true
+        },
+        {
+          title: "Deklaracja PIT-2",
+          checked: false
+        },
+        {
+          title: "Dane personalne",
+          checked: true
+        },
+        {
+          title: "Oświadczenie dla Urzędu Skarbowego",
+          checked: false
+        },
+        {
+          title: "Oświadczenie zleceniobiorcy",
+          checked: true
+        },
+        {
+          title: "Kwestionariusz osobowy",
+          checked: false
+        },
+        {
+          title: "Oświadczenie na rozłąkę",
+          checked: true
+        },
+        {
+          title: "Dotychczasowe świadectwa pracy",
+          checked: false
+        },
+        {
+          title: "Dyplomy",
+          checked: true
+        },
+        {
+          title: "Dokumentacja od lekarza tj. badania okresowe i psychologiczne pod kątem prowadzenia auta",
+          checked: false
         }
-        // "Zakres obowiązków",
-        // "Informacja dla praacownika",
-        // "Deklaracja PIT-2",
-        // "Dane personalne",
-        // "Oświadczenie dla Urzędu Skarbowego",
-        // "Oświadczenie zleceniobiorcy",
-        // "Kwestionariusz osobowy",
-        // "Oświadczenie na rozłąkę",
-        // "Dotychczasowe świadectwa pracy",
-        // "Dyplomy",
-        // "Dokumentacja od lekarza tj. badania okresowe i psychologiczne pod kątem prowadzenia auta"
       ]
     }
   },
@@ -73,4 +128,5 @@ export default {
   .none {
     text-decoration: none;
   }
+
 </style>
