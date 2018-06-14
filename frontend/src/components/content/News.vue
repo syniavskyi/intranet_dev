1031dfd318f7855576a9bb29d54c0bb2fd94d766
dolaron <damianjankowski111@gmail.com>
2018-06-14 16:28:24 +0200

    Added Menu to News and Information
    also removed jumping of menu sidebar when switching between tabs

--- frontend/src/components/content/News.vue
+++ frontend/src/components/content/News.vue
@@ -1,15 +1,28 @@
 <template>
-<div id="news">
-  <div class="plane-dashboard-parent">
-            <div class="plane-dashboard">
-                <div class="plane-dashboard-nav-and-content">
-                    <div class="dashboard-content">
-              <p>NEWS</p>
-           </div>
+<div class="plane-component">
+    <div class="component-nav-and-content">
+      <app-menu></app-menu>
+      <div class="component-content">
+        <div class="content-header">
+          <div class="content-header-title-and-menu">
+            <img src="../../assets/images/nav/if_menu-32.png" width="32px" class="content-header-menu">
+            <p class="content-header-title">Newsy</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
+
+<script>
+import Menu from '../Menu.vue'
+  export default {
+    components: {
+        'app-menu': Menu
+    }
+  }
+</script>
+
+
 <style>
 </style>
