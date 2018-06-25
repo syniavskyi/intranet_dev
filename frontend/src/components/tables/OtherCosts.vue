a2f69a00dd7003460d9d5b165434a7b6acd79ebb
dolaron <damianjankowski111@gmail.com>
2018-06-25 16:23:03 +0200

    Added Disabled inputs and selects in Delegations
    Also some additional styling to tables.
    There are no disabled Table select and inputs in this merge.

--- frontend/src/components/tables/OtherCosts.vue
+++ frontend/src/components/tables/OtherCosts.vue
@@ -5,6 +5,7 @@
                  <div class="delegations-tile-header">
                         <div class="delegations-tile-title">
                             {{ $t("header.otherCosts") }}
+                            <button class="del-add-row" @click="addCostRow"> dodaj wiersz </button>
                         </div>
                         <div class="delegations-tile-underscore"></div>
                     </div>
@@ -13,7 +14,7 @@
                    
                     <div class="delegations-tile-content">
                         <p>20.06.2018</p>
-                        <button @click="addCostRow"> + </button>
+                        
                         <div class="delegations-table-wrapper">
                             <div class="delegations-table-2">
                                 <div class="del-thead-2">
