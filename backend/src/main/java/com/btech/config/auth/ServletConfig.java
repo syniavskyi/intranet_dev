b10bfbb545ca34cfbb8c1ec657610c05d9efec91
Roman Protas <r.protas93@gmail.com>
2018-06-27 14:47:22 +0200

    - Backend commit
     - Server's port customizer bean

--- backend/src/main/java/com/btech/config/auth/ServletConfig.java
+++ backend/src/main/java/com/btech/config/auth/ServletConfig.java
@@ -2,7 +2,9 @@ package com.btech.config.auth;
 
 import com.btech.InitApp;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 public class ServletConfig extends SpringBootServletInitializer {
 
@@ -11,4 +13,13 @@ public class ServletConfig extends SpringBootServletInitializer {
 		return application.sources(InitApp.class);
 	}
 
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return (container -> {
+			container.setContextPath("");
+			if (System.getenv("PORT") != null) {
+				container.setPort(Integer.valueOf(System.getenv("PORT")));
+			}
+		});
+	}
 }
