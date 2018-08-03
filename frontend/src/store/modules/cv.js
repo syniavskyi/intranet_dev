5e2899aa9b19d0078034d614f5608025a6d9cc16
marysiaharcej <marysia.harcej@gmail.com>
2018-08-03 13:14:18 +0200

    Revert "Revert "cleaned code and adjust it to backend""
    This reverts commit 979d75c4f10c1ee30370cb4c4dceea792a98fe65.

--- frontend/src/store/modules/cv.js
+++ frontend/src/store/modules/cv.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const state = {
-    userInfo: {},
     cvElements: {
         photo: true,
         address: true,
@@ -15,31 +14,13 @@ const state = {
 };
 
 const mutations = {
-    GET_USER_INFO(state, data) {
-        state.userInfo = data;
-    },
     SET_CV_ELEMENTS(state, data){
         state.cvElements = data
     }
 };
 
 const actions = {
-    getUserInfo({ commit }) {
-        axios({
-            method: 'GET',
-            url: 'Users' + '(' + "'KBO'" + ')',
-            auth: {
-                username: 'psy',
-                password: 'ides01'
-            },
-            headers: {
-                "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
-            }
-        }).then(res => {
-            let oUserData = res.data.d;
-            commit('GET_USER_INFO', oUserData);
-        }).catch(error =>{ })
-    },
+
     selectAllCvElements({getters, commit}) {
         let elements = getters.getCvElements,
             lang = elements.language,
@@ -75,9 +56,6 @@ const actions = {
 };
 
 const getters = {
-    userInfo(state) {
-        return state.userInfo;
-    },
     getCvElements(state){
         return state.cvElements
     }
