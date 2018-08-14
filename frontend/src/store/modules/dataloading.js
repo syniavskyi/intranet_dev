3f4b9d2aa65b9b88353bd633a9a2d7da6d711ae8
sylwcia <sylwia.piersiak@btech.pl>
2018-08-14 13:50:10 +0200

    skills in profile

--- frontend/src/store/modules/dataloading.js
+++ frontend/src/store/modules/dataloading.js
@@ -13,6 +13,7 @@ const state = {
   studyTypes: [],
   academicTitles: [],
   langLevels: [],
+  fullLanguageList: [],
   sapDomains: ["ZINTRANET_DEPARTMENT", "ZINTRANET_BRANCH", "ZINTRANET_STUDIES_TYPES", "ZINTANET_ACADEMIC_TITLES", "ZINTRANET_LANG_LEVEL"]
 };
 
@@ -46,6 +47,9 @@ const mutations = {
   },
   SET_LANG_LEVELS(state,data){
     state.langLevels = data
+  },
+  SET_LANGUAGE_LIST(state, data) {
+    state.fullLanguageList = data;
   }
 };
 
@@ -55,10 +59,11 @@ const actions = {
     state,
     dispatch
   }) {
-    dispatch('getContractorsList')
-    dispatch('getProjectsList')
-    dispatch('getUserData')
-    dispatch('getUsersLists')
+    dispatch('getContractorsList');
+    dispatch('getProjectsList');
+    dispatch('getUserData');
+    dispatch('getUsersLists');
+    dispatch('getAllLanguages');
     for (let i = 0; i < state.sapDomains.length; i++) {
       dispatch('getDomainValues', state.sapDomains[i])
     }
@@ -113,7 +118,7 @@ const actions = {
       }
     }).then(res => {
       let oProjects = res.data.d.results;
-      console.log(res.data.d);
+      // console.log(res.data.d);
       commit('SET_PROJECTS_LIST', oProjects);
     }).catch(error => {
       console.log(error);
@@ -142,11 +147,12 @@ const actions = {
     })
   },
   getUserData({
-    commit
+    commit,
+    dispatch
   }) {
     axios({
       method: 'GET',
-      url: 'Users' + '(' + "'UIO'" + ')' + '?$expand=UserEducations,UserExperiences,UserCvProjects',
+      url: 'Users' + '(' + "'UIO'" + ')' + '?$expand=UserEducations,UserExperiences,UserCvProjects,UserSkills,UserLang',
       auth: {
         username: 'psy',
         password: 'ides01'
@@ -160,6 +166,10 @@ const actions = {
       commit('SET_USER_INFO', oUserData)
       commit('SET_USER_EDUCATION', oUserData.UserEducations.results)
       commit('SET_USER_EXPERIENCE', oUserData.UserExperiences.results)
+      commit('SET_USER_SKILLS', oUserData.UserSkills.results);
+      dispatch('adjustUserSkills');
+      commit('SET_USER_LANGS', oUserData.UserLang.results);
+      dispatch('adjustLang')
     }).catch(error => {
       console.log(error)
     })
@@ -178,6 +188,24 @@ const actions = {
     }).then(res => {
       commit('GET_USER_LIST', res.data.d.results);
     }).catch(error => { })
+  },
+  getAllLanguages({commit}) {
+    axios({
+      method: 'GET',
+      url: 'languages',
+      auth: {
+        username: 'psy',
+        password: 'ides01'
+      },
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+      }
+    }).then(res => {
+      console.log(res.data.d.results);
+      commit('SET_LANGUAGE_LIST', res.data.d.results);
+    }).catch(error => { })
+    let a = 43;
+    a += 2;
   }
 };
 
@@ -189,7 +217,7 @@ const getters = {
     return state.branches;
   },
   projectsList(state) {
-    return state.projectsList
+    return state.projectsList;
   },
   usersList(state) {
     return state.userList;
@@ -198,7 +226,7 @@ const getters = {
     return state.contractorsList;
   },
   isDataLoaded(state) {
-    return state.isLoaded
+    return state.isLoaded;
   },
   getUserInfo(state) {
     return state.userInfo;
@@ -207,10 +235,13 @@ const getters = {
     return state.studyTypes;
   },
   academicTitles(state) {
-    return state.academicTitles
+    return state.academicTitles;
   },
   langLevels(state){
-    return state.langLevels
+    return state.langLevels;
+  },
+  fullLanguageList(state) {
+    return state.fullLanguageList;
   }
 };
 
