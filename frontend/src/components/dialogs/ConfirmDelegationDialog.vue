af7e3c1a5e2c20a78f555df59d5df21ce2a73122
marysiaharcej <marysia.harcej@gmail.com>
2018-08-21 11:55:06 +0200

    added styles for modal in delegation

--- frontend/src/components/dialogs/ConfirmDelegationDialog.vue
+++ frontend/src/components/dialogs/ConfirmDelegationDialog.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        <button @click="hideDialog"> X </button>
-        <p>Czy chcesz zatwierdzić delegację? Upewnij się, że wprowadzone dane są prawidłowe.</p>
-        <p v-if="!isSuccess">Pomyślnie zapisano dane delegacji</p>
-        <p v-if="isSuccess">Niepowodzenie podczas zapisu numeru delegacji</p>
-        <button @click="saveDelegationNumber">Tak</button>
-        <button @click="hideDialog">Nie, chcę wprowadzić zmiany</button>
-        
+<div>
+    <div class="modal-new">
+        <div class="modal-content">
+            <button @click="hideDialog"> X </button>
+            <p>Czy chcesz zatwierdzić delegację? Upewnij się, że wprowadzone dane są prawidłowe.</p>
+            <p v-if="!isSuccess">Pomyślnie zapisano dane delegacji</p>
+            <p v-if="isSuccess">Niepowodzenie podczas zapisu numeru delegacji</p>
+            <button @click="saveDelegationNumber">Tak</button>
+            <button @click="hideDialog">Nie, chcę wprowadzić zmiany</button>
+        </div>
     </div>
+    <div class="modal-overlay"></div> 
+</div>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex';
