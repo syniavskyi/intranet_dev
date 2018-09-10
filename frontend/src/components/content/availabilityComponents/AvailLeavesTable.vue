c384f00ee59d911a2723e13ebdbf616b039711ad
marysiaharcej <marysia.harcej@gmail.com>
2018-09-10 15:56:10 +0200

    availability - changed structure, added data, validation and filters

--- frontend/src/components/content/availabilityComponents/AvailLeavesTable.vue
+++ frontend/src/components/content/availabilityComponents/AvailLeavesTable.vue
@@ -6,14 +6,17 @@
                 <h2>Zestawienie dyspozycyjności</h2>
                 <div class="availability-tile-underscore"></div>
             </div>
+             <button class="profile-edit-btn" v-if="!editMode"  @click="edit">{{ $t("button.edit") }}</button>
+             <button class="profile-edit-btn-e" v-if="editMode" @click="cancel"><span class="prof-btn-txt">{{ $t("button.finishEdit") }}</span><span class="prof-btn-icon">&#10004;</span></button>
         </div>
         <div class="availability-tile-content">
-            <div class="ava-table-s" v-for="(avail, index) in userAvail" :key="index">
+            <div class="ava-table-s" v-for="(avail, index) in filteredUserAvail" :key="index">
                 <div class="ava-thead-s">
                     <div class="ava-ths-item">Rodzaj wpisu</div>
                     <div class="ava-ths-item">Od</div>
                     <div class="ava-ths-item">Do</div>
                     <div class="ava-ths-item">Status</div>
+                    <div class="ava-ths-item">Opcje</div>
                 </div>
                 <div class="ava-tbody-s">
                     <div class="ava-tbs-item">
@@ -21,22 +24,37 @@
                          <select v-if="!editMode" class="selectProfile selectDisabled" v-model="avail.TypeId">
                             <option v-for="type in availTypes" :key="type.Key" :value="type.Key">{{type.Value}}</option>
                         </select>
+                        <select v-if="editMode" class="selectProfile selectEdit" v-model="avail.TypeId">
+                            <option v-for="type in filteredAavailTypes" :key="type.Key" :value="type.Key">{{type.Value}}</option>
+                        </select>
                     </div>
                     <div class="ava-tbs-item">
                         <div class="ava-tbs-ititle">Od</div>
-                        <v-date-picker class="prof-input-date" popoverDirection="top" @input="validateDates(index)" is-expanded mode="single" v-model="avail.DateStart">
+                        <p class="prof-date-label" v-if="!editMode"> {{ formatDate(avail.DateStart) }} </p>
+                        <v-date-picker v-if="editMode" class="prof-input-date" popoverDirection="top" @input="validateDates(index)" is-expanded mode="single" v-model="avail.DateStart">
                             <input value="avail.DateStart" />
                         </v-date-picker>
                     </div>
                     <div class="ava-tbs-item">
                         <div class="ava-tbs-ititle">Do</div>
-                        <div class="ava-tbs-itxt">{{avail.DateEnd}}</div>
+                        <p class="prof-date-label" v-if="!editMode"> {{ formatDate(avail.DateEnd) }} </p>
+                        <v-date-picker v-if="editMode" class="prof-input-date" popoverDirection="top" @input="validateDates(index)" is-expanded mode="single" v-model="avail.DateEnd">
+                            <input value="avail.DateEnd" />
+                        </v-date-picker>
                     </div>
                     <div class="ava-tbs-item">
                         <div class="ava-tbs-ititle">Status</div>
-                         <select class="selectProfile selectDisabled" v-model="avail.StatusId">
+                        <select v-if="editMode" class="selectProfile selectEdit" v-model="avail.StatusId">
                             <option v-for="status in availStatus" :key="status.Key" :value="status.Key">{{status.Value}}</option>
                         </select>
+                         <select v-if="!editMode" class="selectProfile selectDisabled" v-model="avail.StatusId">
+                            <option v-for="status in availStatus" :key="status.Key" :value="status.Key">{{status.Value}}</option>
+                        </select>
+                    </div>
+                    <div class="ava-tbs-item">
+                        <div class="ava-tbs-ititle">Opcje</div>
+                         <button v-if="editMode">Zapisz</button>
+                         <button v-if="editMode">Usuń</button>
                     </div>
                 </div>
             </div>
@@ -45,41 +63,80 @@
 </template>
 <script>
 import {mapGetters, mapActions} from 'vuex'
+import moment from 'moment'
 export default {
+    props: ['selected-type', 'selected-status'],
+    data () {
+        return {
+            invalidDates: false,
+            editMode: false,
+            _beforeEditingCache: null       
+        }
+    },
     computed: {
         ...mapGetters({
             userAvail: 'getUserAvail',
             availTypes: 'getAvailType',
             availStatus: 'getAvailStatus'
-        })
+        }),
+        filteredUserAvail() {
+            let aFilteredAvail = this.userAvail,
+                sType = this.selectedType,
+                sStatus = this.selectedStatus,
+                fnFilter;
+            
+           
+           if(sType === null && sStatus === null){
+               return aFilteredAvail
+           } else {
+                if(sType && sStatus){ 
+                    fnFilter = function(oItem){
+                        return oItem.TypeId === sType && oItem.StatusId === sStatus;
+                    }
+                }else if (sType){
+                    fnFilter = function(oItem){
+                        return oItem.TypeId === sType;
+                    }
+                }else if(sStatus){
+                    fnFilter = function(oItem){
+                        return oItem.StatusId === sStatus;
+                    }
+                }
+                aFilteredAvail = aFilteredAvail.filter(fnFilter)
+                return aFilteredAvail
+            }
+            
+        }
     },
     methods: {
-        formatAvailType(type){
-            for(let i =0; i<this.availTypes.length; i++){
-                if (this.availTypes[i].Key === type) {
-                    return this.availTypes[i].Value
-                }
-            }
+        ...mapActions(["removeUserAvail"]),
+         edit() {
+            this.editMode = true;
+            this._beforeEditingCache = JSON.parse(JSON.stringify(this.userAvail));
         },
-        formatAvailStatus(status){
-            for(let i =0; i<this.availStatus.length; i++){
-                if (this.availStatus[i].Key === status) {
-                    return this.availStatus[i].Value
-                }
-            }
+        remove(index) {
+            this._beforeEditingCache.splice(index, 1);
+            this.removeUserAvail(index);
+        },
+        cancel() {
+            this.$store.commit("SET_USER_AVAIL", this._beforeEditingCache);
+            this.editMode = false;
         },
         validateDates(index) {
-            // const startDate = this.userExperience[index].DateStart,
-            //     endDate = this.userExperience[index].DateEnd,
-            //     isCurrent = this.userExperience[index].isCurrent;
+           const startDate = this.userAvail[index].DateStart,
+                endDate = this.userAvail[index].DateEnd
 
-            // if (endDate && startDate && isCurrent === false) {
-            //     const formatStartDate = moment(startDate).format("YYYY-MM-DD"),
-            //     formatEndDate = moment(endDate).format("YYYY-MM-DD");
+            if (endDate && startDate) {
+                const formatStartDate = moment(startDate).format("YYYY-MM-DD"),
+                formatEndDate = moment(endDate).format("YYYY-MM-DD");
 
-            //     this.invalidDates = formatStartDate > formatEndDate ? true : false;
-            // }
-            // this.checkFields(index);
+                this.invalidDates = formatStartDate  > formatEndDate ? true : false;
+            }
+        },
+        formatDate(date) {
+            return date !== null && date !== undefined
+            ? moment(date).format("DD.MM.YYYY")
+            : "-";
         },
     }
 }
--- frontend/src/components/content/availabilityComponents/AvailProjectsTable.vue
+++ frontend/src/components/content/availabilityComponents/AvailProjectsTable.vue
@@ -6,6 +6,8 @@
                 <h2>Zestawienie projektów</h2>
                 <div class="availability-tile-underscore"></div>
             </div>
+            <button class="profile-edit-btn" v-if="!editMode"  @click="edit">{{ $t("button.edit") }}</button>
+             <button class="profile-edit-btn-e" v-if="editMode" @click="cancel"><span class="prof-btn-txt">{{ $t("button.finishEdit") }}</span><span class="prof-btn-icon">&#10004;</span></button>
         </div>
         <div class="availability-tile-content">
             <div class="ava-proj-table">
@@ -18,60 +20,51 @@
                     <div class="ava-thproj-item">Status</div>
                     <div class="ava-thproj-item">Uwagi</div>
                 </div>
-                <div class="ava-proj-tbody">
+                <div class="ava-proj-tbody" v-for="(project, index) in filteredUserProjects" :key="index">
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Rodzaj wpisu</div>
-                        <div class="ava-tbproj-itxt">Projekt</div>
+                        <p>{{formattedProjectType()}}</p>
                     </div>
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Nazwa projektu</div>
-                        <div class="ava-tbproj-itxt">Orange</div>
+                        <select v-if="!editMode" class="selectProfile selectDisabled" v-model="project.ProjectId">
+                            <option v-for="proj in allProjects" :key="proj.ProjectId" :value="proj.ProjectId">{{proj.ProjectName}}</option>
+                        </select>
+                        <select v-if="editMode" class="selectProfile selectEdit" v-model="project.ProjectId">
+                            <option v-for="proj in allProjects" :key="proj.ProjectId" :value="proj.ProjectId">{{proj.ProjectName}}</option>
+                        </select>
                     </div>
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Obłożenie</div>
-                        <div class="ava-tbproj-itxt">76%</div>
+                        <p v-if="!editMode">{{project.Engag}}</p>
+                        <div v-if="editMode">
+                            <input required class="ava-input-range-perc" v-model="project.Engag" @input="validateNewEngag(index)" type="number" min="0" max="100" /><span class="ava-perc-span">%</span>
+                            <span class="ava-div-bar"></span>
+                            <label class="ava-input-label-cool">{{ $t("label.engag") }}</label>
+                        </div>
                     </div>
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Od</div>
-                        <div class="ava-tbproj-itxt">01.06.2018</div>
+                        <p class="prof-date-label" v-if="!editMode"> {{ formatDate(project.StartDate) }} </p>
+                        <v-date-picker v-if="editMode" class="prof-input-date" popoverDirection="top" @input="validateDates(index)" is-expanded mode="single" v-model="project.StartDate">
+                            <input value="project.StartDate" />
+                        </v-date-picker>
                     </div>
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Do</div>
-                        <div class="ava-tbproj-itxt">30.12.2018</div>
+                        <p class="prof-date-label" v-if="!editMode"> {{ formatDate(project.EndDate) }} </p>
+                        <v-date-picker v-if="editMode" class="prof-input-date" popoverDirection="top" @input="validateDates(index)" is-expanded mode="single" v-model="project.EndDate">
+                            <input value="project.EndDate" />
+                        </v-date-picker>
                     </div>
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Status</div>
-                        <div class="ava-tbproj-itxt">Planowany</div>
-                    </div>
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Uwagi</div>
-                        <div class="ava-tbproj-itxt"><textarea>Dostęp dzielony z Pavlo (brak tokena)</textarea></div>
-                    </div>
-                </div>
-                <div class="ava-proj-tbody">
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Rodzaj wpisu</div>
-                        <div class="ava-tbproj-itxt">Projekt</div>
-                    </div>
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Nazwa projektu</div>
-                        <div class="ava-tbproj-itxt">Orange</div>
-                    </div>
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Obłożenie</div>
-                        <div class="ava-tbproj-itxt">76%</div>
-                    </div>
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Od</div>
-                        <div class="ava-tbproj-itxt">01.06.2018</div>
-                    </div>
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Do</div>
-                        <div class="ava-tbproj-itxt">30.12.2018</div>
-                    </div>
-                    <div class="ava-tbproj-item">
-                        <div class="ava-tbproj-ititle">Status</div>
-                        <div class="ava-tbproj-itxt">Planowany</div>
+                        <select v-if="editMode" class="selectProfile selectEdit" v-model="project.StatusId">
+                            <option v-for="status in availStatus" :key="status.Key" :value="status.Key">{{status.Value}}</option>
+                        </select>
+                         <select v-if="!editMode" class="selectProfile selectDisabled" v-model="project.StatusId">
+                            <option v-for="status in availStatus" :key="status.Key" :value="status.Key">{{status.Value}}</option>
+                        </select>
                     </div>
                     <div class="ava-tbproj-item">
                         <div class="ava-tbproj-ititle">Uwagi</div>
@@ -83,8 +76,86 @@
     </div>                
 </template>
 <script>
+import {mapGetters, mapActions} from 'vuex'
+import moment from 'moment'
 export default {
-    
+    props: ['selected-type', 'selected-status'],
+    data () {
+        return {
+            invalidDates: false,
+            editMode: false,
+            _beforeEditingCache: null     
+        }
+    },
+    computed: {
+        ...mapGetters({
+            userProjects: 'userProjectsList',
+            availTypes: 'getAvailType',
+            availStatus: 'getAvailStatus',
+            allProjects: 'projectsList'
+        }),
+
+        filteredUserProjects() {
+            let aFilteredProjets = this.userProjects,
+                sType = this.selectedType,
+                fnFilter;
+            
+           
+           if(sType === null){
+               return aFilteredProjets
+           } else {
+               aFilteredProjets = aFilteredProjets.filter(function(oItem){
+                  return oItem.TypeId === sType;
+              })
+            return aFilteredProjets
+            }
+            
+        }
+    },
+    methods: {
+        ...mapActions(["removeUserProject"]),
+         edit() {
+            this.editMode = true;
+            this._beforeEditingCache = JSON.parse(JSON.stringify(this.userProjects));
+        },
+        remove(index) {
+            this._beforeEditingCache.splice(index, 1);
+            this.removeUserProject(index);
+        },
+        cancel() {
+            this.$store.commit("SET_USER_AVAIL_PROJECTS", this._beforeEditingCache);
+            this.editMode = false;
+        },
+        validateDates(index) {
+           const startDate = this.userProjects[index].DateStart,
+                endDate = this.userProjects[index].DateEnd
+
+            if (endDate && startDate) {
+                const formatStartDate = moment(startDate).format("YYYY-MM-DD"),
+                formatEndDate = moment(endDate).format("YYYY-MM-DD");
+
+                this.invalidDates = formatStartDate  > formatEndDate ? true : false;
+            }
+        },
+        formatDate(date) {
+            return date !== null && date !== undefined
+            ? moment(date).format("DD.MM.YYYY")
+            : "-";
+        },
+        validateNewEngag(index){
+            let project = userProjects[index]
+            if (project.Engag < 0) project.Engag = null;
+            if (project.Engag > 100) project.Engag = 100;
+        },
+        formattedProjectType() {
+            for(let i = 0; i < this.availTypes.length; i++) {
+                if (this.availTypes[i].Key === "PR") {
+                    return this.availTypes[i].Value
+                }
+            }
+        },
+    }
 }
 </script>
+
 
