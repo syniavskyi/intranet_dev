413d9c99e849edf33635ec75b075cf4fb58ccb77
sylwcia <sylwia.piersiak@btech.pl>
2018-09-24 09:55:44 +0200

    changed geeter name

--- frontend/src/components/content/CV.vue
+++ frontend/src/components/content/CV.vue
@@ -329,7 +329,7 @@ export default {
       fieldOfStudyDesc: 'getFieldOfStudyDescList',
       workPositions: 'getWorkPositions',
       fullLanguageList: 'getFullLanguageList',
-      langLevels: 'langLevels'
+      langLevels: 'getLangLevels'
 
     })
   }
