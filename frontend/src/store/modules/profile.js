7fde941ea1dd6c624a0731eab09513915de1c7f4
awisniewska <tawisniewska@gmail.com>
2018-10-26 11:37:45 +0200

    Small changes in logout and dataloading
    -Clear authorization data after logout,
    -Change queue of dataloading

--- frontend/src/store/modules/profile.js
+++ frontend/src/store/modules/profile.js
@@ -26,7 +26,7 @@ const state = {
     }
   ],
   showChangePasswordDialog: false,
-  dataToRead: ["NewToken", "Domains", "UserData", "UserList", "Industries", "Contractors", "Projects", "Languages", "SchoolDesc", "FieldOfStudy"]
+  dataToRead: ["NewToken", "Domains", "Industries", "UserData", "UserList", "Contractors", "Projects", "Languages", "SchoolDesc", "FieldOfStudy"]
 };
 
 const mutations = {
