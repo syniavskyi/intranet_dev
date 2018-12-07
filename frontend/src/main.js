f7db5a54e683a884c8d2e7f70f24e2e77febef04
Pavlo <syniavskyi.pavlo@gmail.com>
2018-12-07 11:38:40 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -47,9 +47,9 @@ Vue.config.productionTip = false
 
 // axios.defaults.baseURL = 'http://10.0.2.6:8080/';
 // axios.defaults.baseURL = 'https://btech-intranet.herokuapp.com/';
-// axios.defaults.baseURL = 'http://10.0.2.5:8080/';
+axios.defaults.baseURL = 'http://46.229.158.46:8050/sap/opu/odata/sap/ZGW_INTRANET_SRV/';
 // axios.defaults.baseURL = 'http://nw5.local.pl:8050/sap/opu/odata/sap/ZGW_INTRANET_SRV/';
-axios.defaults.baseURL = '/api/sap/opu/odata/sap/ZGW_INTRANET_SRV/';
+// axios.defaults.baseURL = '/api/sap/opu/odata/sap/ZGW_INTRANET_SRV/';
 
 odata().config({
     endpoint: 'http://nw5.local.pl:8050/sap/opu/odata/sap/ZGW_INTRANET_SRV/',
